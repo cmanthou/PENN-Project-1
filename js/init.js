79764262c89c27f07df48c9d266dd8c7fece2d36
Conchettapark <63162177+Conchettapark@users.noreply.github.com>
2020-07-02 22:00:48 -0400

    Feature/api (#14)
    * new-title
    
    * api
    
    Co-authored-by: Conchetta Park <conchettapark@MacBook-Pro.local>

--- js/init.js
+++ js/init.js
@@ -1,8 +1,50 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
+(function ($) {
+  $(function () {
+    $(".sidenav").sidenav();
+    $(".parallax").parallax();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+var token;
+var queryURL =
+  "https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/oauth2/token";
+$.ajax({
+  url: queryURL,
+  method: "POST",
+  data: {
+    grant_type: "client_credentials",
+    client_id: "jwiHAizfWoRDRFwMQBD46rrrt7RaSXvNMjMDfgDuae8O7eFIEj",
+    client_secret: "ME14Jzyaaa4mwLFclDqoVZuw1GFPexThK5Y7zVVs",
+  },
+}).then(function (response) {
+  console.log(response);
+  token = response.access_token;
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".carousel");
+  var options = [];
+  var instances = M.Carousel.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function () {
+  $(".carousel").carousel();
+});
+
+var queryURL = `https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/organizations`;
+
+$(".btn").on("click", function (event) {
+  event.preventDefault();
+  console.log("btn click");
+
+  $.ajax({
+    url: queryURL,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
+});
